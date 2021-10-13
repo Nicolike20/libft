e3d0c6b7f424698e92960c6e5a5402b9df0dcffd
Nicolike <80271134+Nicolike20@users.noreply.github.com>
2021-10-13 17:35:58 +0200

    commit w/ everything

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nortolan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 16:31:05 by nortolan          #+#    #+#             */
+/*   Updated: 2021/09/17 17:15:42 by nortolan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (lst == NULL || (*del) == NULL)
+		return ;
+	while (*lst)
+	{
+		(*del)((*lst)->content);
+		temp = *lst;
+		*lst = (*lst)->next;
+		free(temp);
+	}
+}
---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nortolan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 13:13:45 by nortolan          #+#    #+#             */
+/*   Updated: 2021/09/17 14:59:13 by nortolan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*res;
+
+	res = malloc(sizeof(t_list));
+	if (res == NULL)
+		return (NULL);
+	res->content = content;
+	res->next = NULL;
+	return (res);
+}
